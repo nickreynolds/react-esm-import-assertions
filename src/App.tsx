9bb1e613c35db6745e082c90ba7ee979e5d0a83f
nickreynolds <nicholas.s.reynolds@gmail.com>
2022-09-13 14:01:34 -0400

    add json

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css' assert { type: 'css' }
+import test from "./test.json" assert { type: 'json' }
 
 function App() {
+  console.log("test: ", test)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Uses Import Assertion to Load JSON
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          {`test: ${JSON.stringify(test)}`}
+        </p>
       </header>
     </div>
   );
